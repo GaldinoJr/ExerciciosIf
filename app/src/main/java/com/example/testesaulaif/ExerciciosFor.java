617546b803266670062f3abeca44c6ed9d2fb5ad
grodrigues <gjunior@vidalink.com.br>
2020-07-07 23:55:59 -0300

    Testes do exercício da velocidade

---
+++ app/src/main/java/com/example/testesaulaif/ExerciciosFor.java
@@ -0,0 +1,33 @@
+package com.example.testesaulaif;
+
+public class ExerciciosFor {
+    /**
+     * 1 - Faça um programa que receba um número inteiro, conte mais 10 (usando for) e
+     * devolva o resultado
+     */
+
+    /**
+     * 2 - Faça um programa que receba um número inteiro, conte mais 20 (usando for), concatene cada
+     * número em uma string, e devolva essa string
+     */
+
+    /**
+     * 3 - Faça um programa que receba um numero inteiro, multiplique esse número por ele mesmo
+     * 4 vezez (usando for), e devolva o resultado
+     */
+
+    /**
+     * 3 - Faça um programa que receba um numero final, execulte o for até esse número, concatene cada
+     *número em uma string, e devolva essa string
+     */
+
+    /**
+     * 4 - Faça um programa que conctene em uma string somente os numeros impares de 0 até 20,
+     * e devolva essa string
+     */
+
+    /**
+     * 5 - Faça um programa que conctene em uma string somente os numeros pares de 0 até 20,
+     * e devolva essa string
+     */
+}
